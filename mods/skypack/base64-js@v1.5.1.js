b4b1f04eff484c0997adf04f8bcb6833c62ae008
taiga morikawa <65807636+taigamorikawa@users.noreply.github.com>
2023-01-18 13:43:56 +0900

    Delete mods/skypack directory

--- mods/skypack/base64-js@v1.5.1.js
+++
@@ -1,9 +0,0 @@
-/*
- * Skypack CDN - base64-js@1.5.1
- * Remove "min" from url for additional help & instructions.
- */
-
-export * from '/-/base64-js@v1.5.1-W9sqY0mF5INkRg7HAvxn/dist=es2020,mode=imports,min/optimized/base64-js.js';
-export {default} from '/-/base64-js@v1.5.1-W9sqY0mF5INkRg7HAvxn/dist=es2020,mode=imports,min/optimized/base64-js.js';
-
-// Perf Optimization: Preload Deep Imports
--- mods/skypack/ieee754@v1.2.1.js
+++
@@ -1,9 +0,0 @@
-/*
- * Skypack CDN - ieee754@1.2.1
- * Remove "min" from url for additional help & instructions.
- */
-
-export * from '/-/ieee754@v1.2.1-wxdRuKvQQOTpW1dpWzFI/dist=es2020,mode=imports,min/optimized/ieee754.js';
-export {default} from '/-/ieee754@v1.2.1-wxdRuKvQQOTpW1dpWzFI/dist=es2020,mode=imports,min/optimized/ieee754.js';
-
-// Perf Optimization: Preload Deep Imports
